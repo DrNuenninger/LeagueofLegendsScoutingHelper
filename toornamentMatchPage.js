74dadb39949fa4d7165fe6dab42f9426f1ce4a27
DrNuenninger <tillmann@nuenninghoff.de>
2019-11-21 22:07:25 +0100

    Make op.gg Links avaiable for more toornaments

--- toornamentMatchPage.js
+++ toornamentMatchPage.js
@@ -2,15 +2,32 @@ teamsDiv = document.getElementsByClassName("layout-block content")[0];
 team1Div = teamsDiv.getElementsByClassName("size-1-of-2")[0];
 team2Div = teamsDiv.getElementsByClassName("size-1-of-2")[1];
 
-team1Div.firstElementChild.firstElementChild.appendChild(createUrlElement(team1Div.firstElementChild.lastElementChild));
-team2Div.firstElementChild.firstElementChild.appendChild(createUrlElement(team2Div.firstElementChild.lastElementChild));
+if (team1Div != undefined) {
+    opggElementTeam1 = createUrlElement(team1Div.firstElementChild.lastElementChild);
+    if (opggElementTeam1 != null) {
+        team1Div.firstElementChild.firstElementChild.appendChild(opggElementTeam1);
+    }
+}
+
+if (team2Div != undefined) {
+    opggElementTeam2 = createUrlElement(team2Div.firstElementChild.lastElementChild);
+    if (opggElementTeam2 != null) {
+        team2Div.firstElementChild.firstElementChild.appendChild(opggElementTeam2);
+    }
+}
 
 function createUrlElement(playersDiv) {
     var opggUrlElementContainer = document.createElement("div");
     var opggUrlElement = document.createElement("a");
     var linkText = document.createTextNode("Team op.gg Link");
     var summonerNames = loadSummonerNames(playersDiv);
-    var opggUrl = createTeamOpggUrl(summonerNames);
+
+    if (summonerNames.length > 0) {
+        var opggUrl = createTeamOpggUrl(summonerNames);
+    } else {
+        return null;
+    }
+    
 
     opggUrlElement.href = opggUrl;
     opggUrlElement.rel = "noopener noreferrer"
@@ -31,10 +48,14 @@ function loadSummonerNames(playersDiv) {
         var childDivs = infoDivs[index].firstElementChild.childNodes;
         
 
-        if (childDivs.length == 0) {continue;}
+        if (childDivs.length != 2) {continue;}
 
+        var summonerName = childDivs[0].innerText;
         var unsanitizedSummonerName = childDivs[1].innerText;
-        var summonerName = unsanitizedSummonerName.replace("Summoner Name:", "");
+        if (unsanitizedSummonerName != "No information on this player") {
+            summonerName = unsanitizedSummonerName.replace("Summoner Name:", "");
+        }
+        
         summonerNames.push(summonerName);
     }
 
