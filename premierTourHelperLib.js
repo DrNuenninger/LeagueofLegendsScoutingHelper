1662002cef5d41ab77b323a3f9b34535b3f9e290
Dr Nuenninger <tillmann@nuenninghoff.de>
2024-06-01 17:56:50 +0200

    Update repo to current version "1.3.4"

--- premierTourHelperLib.js
+++ premierTourHelperLib.js
@@ -1,7 +1,8 @@
 function createTeamOpggUrl(summonerNames) {
     var opggUrl = "https://euw.op.gg/multi/query=";
     summonerNames.forEach(summonerName => {
-        opggUrl = opggUrl + "%2C"+ summonerName;
+		encodedSummonerName = summonerName.replace("#", "%23")
+        opggUrl = opggUrl + "%2C"+ encodedSummonerName;
     });
 
     return opggUrl;
--- teamPage.js
+++ teamPage.js
@@ -1,28 +1,45 @@
+var allSummonerNames = [];
+var activeSummonerNames = [];
+
 playersDiv = document.getElementsByClassName("content-portrait-grid-l")[0];
 playersDivParent = playersDiv.parentNode;
-
 playersDivParent.insertBefore(createUrlElement(),playersDiv);
 
 function createUrlElement() {
-    var opggUrlElementContainer = document.createElement("div");
-    var opggUrlElement = document.createElement("a");
-    var linkText = document.createTextNode("Team op.gg Link");
-    var summonerNames = loadSummonerNames();
-    var opggUrl = createTeamOpggUrl(summonerNames);
 
-    opggUrlElement.href = opggUrl;
-    opggUrlElement.rel = "noopener noreferrer"
-    opggUrlElement.target = "_blank";
-    opggUrlElement.appendChild(linkText);
+	
+    loadSummonerNames();
+	
+    var opggUrlAll = createTeamOpggUrl(allSummonerNames);
+    var opggUrlActive = createTeamOpggUrl(activeSummonerNames);
+	
+	var opggUrlElementContainer = document.createElement("div");
+    var opggUrlElementActive = document.createElement("a");
+
+    opggUrlElementActive.href = opggUrlActive;
+    opggUrlElementActive.rel = "noopener noreferrer";
+    opggUrlElementActive.target = "_blank";
+    opggUrlElementActive.appendChild(document.createTextNode("Active team op.gg"));
+	opggUrlElementActive.style.paddingRight = "50px";
 
     opggUrlElementContainer.classList.add("txt-content");
-    opggUrlElementContainer.appendChild(opggUrlElement);
+    opggUrlElementContainer.appendChild(opggUrlElementActive);
+	
+	
+	var opggUrlElementAll = document.createElement("a");
+
+    opggUrlElementAll.href = opggUrlAll;
+    opggUrlElementAll.rel = "noopener noreferrer";
+    opggUrlElementAll.target = "_blank";
+    opggUrlElementAll.appendChild(document.createTextNode("Full team op.gg"));
+
+    opggUrlElementContainer.classList.add("txt-content");
+    opggUrlElementContainer.appendChild(opggUrlElementAll);
 
     return opggUrlElementContainer;
 }
 
 function loadSummonerNames() {
-    var summonerNames = [];
     var infoDivs = document.getElementsByClassName("txt-info");
 
     for (var index = 0; index < infoDivs.length; index++) {
@@ -33,10 +50,16 @@ function loadSummonerNames() {
         var title = childDiv.getAttribute("title");
         if (title != null) {
             if (title.includes("LoL Summoner Name")) {
-                summonerNames.push(infoDivs[index].firstElementChild.innerText);
+				var summonerName = infoDivs[index].firstElementChild.innerText;
+                allSummonerNames.push(summonerName);
+				
+				var activePlayerDiv = infoDivs[index].getElementsByClassName("txt-status-positive");
+				if( activePlayerDiv.length > 0 ) {
+					activeSummonerNames.push(summonerName);
+				}
             }
         }
     }
 
-    return summonerNames;
+    return;
 }
